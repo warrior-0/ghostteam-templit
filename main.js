83b2bc1ca4478415b461e132b2ce736249c474db
warrior-0 <bodla0330@gmail.com>
2025-05-22 21:40:58 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
-  if (document.getElementById('menuContainer')) {
-    fetch('menu.html')
-      .then(response => response.text())
-      .then(html => {
-        document.getElementById('menuContainer').innerHTML = html;
-        // 햄버거 메뉴 토글
-        const menuToggle = document.querySelector('.menu-toggle');
-        const sideMenu = document.getElementById('sideMenu');
-        if(menuToggle && sideMenu){
-          menuToggle.addEventListener('click', () => {
-            sideMenu.style.display = sideMenu.style.display === 'flex' ? 'none' : 'flex';
-          });
-          document.querySelectorAll('.toggle-btn').forEach(btn => {
-            btn.addEventListener('click', () => {
-              const targetId = btn.dataset.target;
-              const submenu = document.getElementById(targetId);
-              const isOpen = submenu.style.display === 'flex';
-              submenu.style.display = isOpen ? 'none' : 'flex';
-              btn.textContent = isOpen ? '+' : '−';
-            });
-          });
-        }
-      });
-  }
 
   // 괴담 메뉴 드롭다운 오래 보이게
   const menuLi = document.getElementById('urbanMenu');
